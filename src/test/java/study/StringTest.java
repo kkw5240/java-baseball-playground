49879118bdda508751a6f2d0a232e27dcd5fbb23
kkw5240 <kkw5240@gmail.com>
2022-05-15 13:38:46 +0900

    0. 학습테스트 실습 - StringTest 요구사항 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,48 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
+    @DisplayName("replace Test : abc -> adc")
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    @DisplayName("split & contains Test : \"1,2\" -> [\"1\", \"2\"]")
+    void contains() {
+        String[] actual = "1,2".split(",");
+
+        assertThat(actual).contains("1", "2");
+        assertThat(actual).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("substring Test : (1,2) -> 1,2")
+    void substring() {
+        String actual = "(1,2)".substring(1, 4);
+
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt Test : abc -> b")
+    void charAt() {
+        String given = "abc";
+
+        assertThat(given.charAt(1)).isEqualTo('b');
+
+        assertThatThrownBy(() -> {
+            given.charAt(4);
+
+        }).isInstanceOf(
+                StringIndexOutOfBoundsException.class
+        );
     }
 }
